50d622a8d5fbbde20e413e652809f48550c9a8ea
QuanTT <thaithienquan09@gmail.com>
2025-08-25 23:39:20 +0700

    Third Commit

--- src/NoteApp/NoteAppComponent.tsx
+++ src/NoteApp/NoteAppComponent.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react"
+
 export default function NoteAppComponent(){
-    return(<></>)
+    const [note, setnote] = useState({
+        name:'',
+        email:'',
+        checked:false
+    })
+    const handleChange=(e)=>{
+        const {name, value , type , checked}=e.target;
+setnote((prev)=>({...prev,[name]: type=="checkbox"? checked :value}));
+    }
+    return(<>
+    <div className="">
+        <input type="text" value={note.name} placeholder="Name here" name={note.name} onChange={handleChange}/>
+        <input type="checkbox" checked={note.checked?"Remembered":"Not Remember"} placeholder="Name here" name={note.name} onChange={handleChange}/>
+    </div>
+    </>)
 }
