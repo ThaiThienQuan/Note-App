fef241fa7f9c4ab3cc39929ddd2e3f14a5650831
QuanTT <thaithienquan09@gmail.com>
2025-08-26 21:49:07 +0700

    prepare Note with useContext

---
+++ src/Components/NoteContext.tsx
@@ -0,0 +1,10 @@
+import {createContext, useState} from "react";
+
+const NoteContext=createContext({addNote:()=>{}});
+export const NotePorvider=({children})=>{
+    const [note, setNote] = useState()
+    const addNote=()=>{}
+    const deleteNote=(i)=>{}
+    <NoteContext.Provider value={{addNote(),delete()}}>{children}</NoteContext.Provider>
+}
+                          export default NoteContext;
---
+++ src/Components/NoteForm.tsx
@@ -0,0 +1,7 @@
+export interface Note{
+    id: string;
+    title:string;
+    price:double;
+    available:false;
+    createAt:Date;
+}
