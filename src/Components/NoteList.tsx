1e3c0a9c97963570998df66fccd67efaa95e07b0
QuanTT <thaithienquan09@gmail.com>
2025-09-04 23:40:17 +0700

    Begin setup NoteApp with NoteList.tsx

--- src/Components/NoteList.tsx
+++ src/Components/NoteList.tsx
@@ -1,3 +1,8 @@
-export default function NoteList(){
+import type { Note } from "../types/note";
+interface Props {
+  deleteNote: (note: Note) => void;
+}
+export default function NoteList({deleteNote}:Props){
+    deleteNote();
     return(<></>)
 }
