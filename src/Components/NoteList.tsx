3206b057b0fe053d56067378a83427a75a6820e3
QuanTT <thaithienquan09@gmail.com>
2025-09-12 23:45:20 +0700

    update feature in NoteList

--- src/Components/NoteList.tsx
+++ src/Components/NoteList.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import type { Note } from "../types/note";
 
 interface Props {
   notes: Note[];
   deleteNote: (id: string) => void;
-  updateNote:(id:string, updateNote:Note)=>void;
+  updateNote: (id: string, updateNote: Note) => void;
   filterNote: Note[];
-
 }
 
-export default function NoteList({ notes, deleteNote ,filterNote}: Props) {
+export default function NoteList({ notes, deleteNote, filterNote }: Props) {
+  const [editId, seteditId] = useState<string | null>(null);
+  const [editTitle, seteditTitle] = useState("");
+  const [editContent, seteditContent] = useState("");
   return (
     <div>
       {notes.length == 0 ? (
@@ -19,22 +22,28 @@ export default function NoteList({ notes, deleteNote ,filterNote}: Props) {
             key={note.id}
             className="border p-3 mb-2 rounded shadow-sm bg-gray-50"
           >
-            <p>{note.id}</p>
-            <h2 className="font-bold">{note.title}</h2>
-            <p>{note.content}</p>
-            <p>{note.price}</p>
-            <p>{note.available ? "Saved" : "Not save"}</p>
-            <strong>
-              <small>{new Date(note.createAt).toString()}</small>
-            </strong>
-            <button
-              onClick={() => {
-                deleteNote(note.id);
-              }}
-              className="text-red-500 mt-2"
-            >
-              Delete
-            </button>
+            {editId === note.id ? (
+              <></>
+            ) : (
+              <>
+                <p>{note.id}</p>
+                <h2 className="font-bold">{note.title}</h2>
+                <p>{note.content}</p>
+                <p>{note.price}</p>
+                <p>{note.available ? "Saved" : "Not save"}</p>
+                <strong>
+                  <small>{new Date(note.createAt).toString()}</small>
+                </strong>
+                <button
+                  onClick={() => {
+                    deleteNote(note.id);
+                  }}
+                  className="text-red-500 mt-2"
+                >
+                  Delete
+                </button>
+              </>
+            )}
           </div>
         ))
       )}
